ea3eb6b058e583dd590a40da660d51853367ecf9
malikarjun birajdar <mbirajdar449@gmail.com>
2023-12-17 13:09:23 +0530

    code changes updating logger

--- src/main/java/com/jjm/studentapp/controller/StudentController.java
+++ src/main/java/com/jjm/studentapp/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.jjm.studentapp.controller;
 import com.jjm.studentapp.entity.Student;
 
 import com.jjm.studentapp.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +11,28 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Slf4j
+
 public class StudentController {
     @Autowired
     StudentService studentservice;
     @PostMapping("/addStudent")
     public String addStudent(@RequestBody Student student){
         studentservice.addStudent(student);
+        log.info("Calling addstudent() method of student controller");
+        log.info("Adding student with id{}",student.getId());
         return "student added successfully";
     }
 @GetMapping("/showAllStudents")
     public List<Student> showAllStudent()
     {
+        log.info("retriving all record student ");
         return studentservice.showAllStudent();
     }
 
     @GetMapping("/getstudentById/{id}")
     public Optional<Student> getStudentById(@PathVariable("id") int id){
+        log.info("showing id student with id{}",id);
         return studentservice.getStudentById(id);
     }
 
@@ -35,12 +42,14 @@ public class StudentController {
     }*/
 @DeleteMapping("deleteById/{id}")
     public String deleteById(@PathVariable("id") int id){
+    log.info("deleting student with id{}",id);
     studentservice.deleteById(id);
     return "record delete for ID:"+id;
 }
 
 @PutMapping ("/updateById/{id}")
     public Student updateStudent(@PathVariable("id") int id ,@RequestBody Student student){
+    log.info("updating student with id{}",student.getId());
     return studentservice.updateStudent(id,student);
 }
 }
--- src/main/java/com/jjm/studentapp/service/StudentServiceImpl.java
+++ src/main/java/com/jjm/studentapp/service/StudentServiceImpl.java
@@ -2,6 +2,7 @@ package com.jjm.studentapp.service;
 
 import com.jjm.studentapp.entity.Student;
 import com.jjm.studentapp.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class StudentServiceImpl implements StudentService{
 
     @Autowired
@@ -30,12 +32,14 @@ public class StudentServiceImpl implements StudentService{
 
     @Override
     public void deleteById(int id) {
+        log.info("deleting student with id{} with datele layer",id);
          studentRepository.deleteById(id);
     }
 
     @Override
     public Student updateStudent(int id, Student student) {
         Student student1=studentRepository.findById(id).get();
+        log.info("updating student with id{} with update layer",student.getId());
         student1.setLastName(student.getFirstName());
         student1.setFirstName(student.getLastName());
         return studentRepository.save(student1);
